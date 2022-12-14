6c9a84844364b7174959f9585d77e7a5c388d575
Junho-06 <k34920032@gmail.com>
2022-12-14 17:45:24 +0900

    update serializer validate def

--- user/serializers.py
+++ user/serializers.py
@@ -6,3 +6,9 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = '__all__'
+
+    def validate(self, attrs):
+        idf = User.objects.filter(idf=attrs['idf'])
+        if idf.exists():
+            raise serializers.ValidationError('이 아이디는 이미 존재합니다.')
+        return attrs
