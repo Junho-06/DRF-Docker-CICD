6b32a97e5f75d7930d68db7d228a124cc7d1aa9c
Junho-06 <k34920032@gmail.com>
2022-12-14 18:06:43 +0900

    update models UserManager

--- user/models.py
+++ user/models.py
@@ -1,7 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin
+from django.utils import timezone
+from django.utils. translation import ugettext_lazy as _
 
-class User(models.Model):
+class UserManager(BaseUserManager):
 
-    name = models.CharField(max_length=10)
-    idf = models.CharField(max_length=15)
-    password = models.CharField(max_length=15)
+    use_in_migrations = True
+
+    def _create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError('이메일은 필수 입력란 입니다.')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+    
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('슈퍼유저는 반드시 is_staff 가 True 여야 합니다.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('슈퍼유저는 반드시 is_superuser 가 True 여야 합니다.')
+
+        return self.create_user(email,password, **extra_fields)
