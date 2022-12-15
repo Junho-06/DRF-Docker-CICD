8eba43a59703536b912a3c570b58da2e82f9a8be
Junho-06 <k34920032@gmail.com>
2022-12-15 17:07:57 +0900

    update refreshview url

--- user/urls.py
+++ user/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path,include
 from .views import RegisterAPIView, AuthView
+from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     path("register/", RegisterAPIView.as_view()),
     path("auth/", AuthView.as_view()),
+    path('auth/refresh/', TokenRefreshView.as_view()),
 ]
