6bc71ecc995f1161e27f8f079a5f7c216b37375e
Junho-06 <k34920032@gmail.com>
2022-12-15 17:06:31 +0900

    update urls auth

--- user/urls.py
+++ user/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path,include
-from .views import RegisterAPIView
+from .views import RegisterAPIView, AuthView
 
 urlpatterns = [
     path("register/", RegisterAPIView.as_view()),
+    path("auth/", AuthView.as_view()),
 ]
